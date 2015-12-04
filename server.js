81b01d686ca3fe1aae606918642c54222774dacc
skultimate5 <sck42@case.edu>
2015-12-03 20:03:38 -0500

    Removing commented setTimeout section

--- server.js
+++ server.js
@@ -655,29 +655,6 @@ app.get('/viewOldLevel/:i', function(req, res){
       });
     }); 
   });
-
-  /*setTimeout(function(){
-    res.render('viewOldLevel', 
-      { levelNum: levelNum,
-      currentLevelNum: oldLevel.currentLevelNum,
-      userID: userID,
-      goal1: {
-        name: oldLevel.goal1Name,
-        percentComplete: 100,
-        leftToGo: "Complete!"
-      },
-      goal2: {
-        name: oldLevel.goal2Name,
-        percentComplete: 100,
-        leftToGo: "Complete!"
-      },
-      goal3: {
-        name: oldLevel.goal3Name,
-        percentComplete: 100,
-        leftToGo: "Complete"
-      },
-    });
-  }, 500);*/
 });
 
 app.get('/achievements', function(req,res){
